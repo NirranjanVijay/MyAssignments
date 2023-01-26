7b39700e5ac24d49af336bd2f6da1eb5e7b276eb
NirranjanVijay <123624269+NirranjanVijay@users.noreply.github.com>
2023-01-26 14:05:44 +0530

    Add files via upload
    week.day1

---
+++ Mobile.java
@@ -0,0 +1,39 @@
+
+public class Mobile {
+	public void sendmessage() {
+		   System.out.println("Send Message");
+	   }
+	  public void playmusic() {
+		  System.out.println("Play Music");
+	  }
+	  public void watchvideos()   {
+		  System.out.println("Watch Videos");
+	  }
+	  public void makecalls() {
+		  System.out.println("Make Calls");
+	  }
+	  public void playgame() {
+		  System.out.println("Play Game");
+	  }
+	  public void usingsocialmedia() {
+		  System.out.println("Using Social Media");
+	  }
+	  public void makevideocalls() {
+		  System.out.println("Make Video Calls");
+	  }
+	  public void watchyoutube() {
+		  System.out.println("Watch youtube");
+	  }
+	public static void main(String[] args) {
+		    Mobile POCO=new Mobile();
+		    POCO.sendmessage();
+		    POCO.playmusic();
+		    POCO.watchvideos();
+		    POCO.makecalls();
+		    POCO.playgame();
+		    POCO.usingsocialmedia();
+		    POCO.makevideocalls();
+		    POCO.watchyoutube();
+	          }
+	}
+
